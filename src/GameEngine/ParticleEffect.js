a2d5eee1d9c788a4489ed9fea472a7ad6a82dbb4
ThatCoolCoder <That-Cool-Coder@github.com>
2021-07-24 17:32:28 +0930

    add option to autodelete ParticleEffects when finished

--- src/GameEngine/ParticleEffect.js
+++ src/GameEngine/ParticleEffect.js
@@ -38,7 +38,7 @@ spnr.GameEngine.ParticleEffect = class extends spnr.GameEngine.Entity {
         super(name, localPosition, localAngle);
         this.emitterData = emitterData;
         this.looping = looping;
-        this.deleteWhenFinished = false;
+        this.deleteWhenFinished = deleteWhenFinished;
     }
 
     play() {
