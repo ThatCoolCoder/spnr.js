d01b3e1aeb03b90f744a27776b2048c6650effa8
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-02-09 09:45:40 +1030

    Docs for misc

--- src/misc.js
+++ src/misc.js
@@ -5,18 +5,29 @@ spnr.uniqueId = function() {
     return '_' + spnr.random().toString(36).substr(2, 9);
 }
 
+/**
+ * Generate a random true or false value
+ * @returns {boolean}
+ */
 spnr.randBoolean = function() {
     // Randomly return true or false
 
     return spnr.random() > 0.5;
 }
 
+/**
+ * Do nothing. Was created before I realised you can just do void(0). Only kept for backwards compatibility
+ */
 spnr.doNothing = function() {
     // do nothing
 }
 
+/**
+ * Like a for loop mixed with a foreach.
+ * @param {number} n - loop from 0 to n - 1.
+ * @param {Function} func - function to run each loop cycle with n as a parameter.
+ */
 spnr.doNTimes = function(n, func) {
-    // Run func n times, with the loop counter as a parameter
     for (var i = 0; i < n; i ++) {
         func(i);
     }
