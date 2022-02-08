88c637c6d5efcb647bcc535fa3a29f875a0e523f
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-02-09 09:59:07 +1030

    Docs for obj

--- src/obj.js
+++ src/obj.js
@@ -1,13 +1,33 @@
+/**
+ * spnr.js Object operations
+ * @namespace
+ */
 spnr.obj = {};
 
+/**
+ * Get an array of the keys of an object.
+ * @param {Object} obj
+ * @returns {string[]} keys of the object
+ */
 spnr.obj.keys = function(obj) {
     return Object.keys(obj);
 }
 
+/**
+ * Get an array of the values of an object.
+ * @param {Object} obj
+ * @returns {string[]} values of the object
+ */
 spnr.obj.values = function(obj) {
     return Object.values(obj);
 }
 
+/**
+ * Change the values of an object without changing the keys.
+ * Assumes that the length of the values is greater than or equal to the amount of keys in the object.
+ * @param {Object} obj
+ * @param {any[]} values - values to set the keys to 
+ */
 spnr.obj.setValues = function(obj, values) {
     // change the values of an object without changing the keys
     // assumes that keys and values are same length, etc
@@ -17,6 +37,11 @@ spnr.obj.setValues = function(obj, values) {
     });
 }
 
+/**
+ * Shallow copy an object (copy one level but don't deep copy properties).
+ * @param {Object} obj - object to copy
+ * @returns {Object}
+ */
 spnr.obj.oneLevelCopy = function(obj) {
     var newObj = {};
     var keys = spnr.obj.keys(obj);
