8750a32b8085f5ecfe110c69753b4ec6dd8f0acc
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-02-12 10:24:23 +1030

    Scene and texture docs

--- src/GameEngine/Scene.js
+++ src/GameEngine/Scene.js
@@ -1,4 +1,15 @@
+/**
+ * Scene class that can be selected and displayed.
+ * @class
+ * @extends {spnr.GameEngine.Entity}
+ */
 spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
+    /**
+     * Create a new scene
+     * @param {string} name 
+     * @param {spnr.Vector} localPosition 
+     * @param {number} localAngle 
+     */
     constructor(name, localPosition=spnr.v(0, 0), localAngle=0) {
         super(name, localPosition, localAngle);
 
@@ -8,11 +19,25 @@ spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
         this.flattenedChildList = [];
     }
 
+    /**
+     * I'm not sure what this does and maybe it's broken
+     * @private
+     */
     get globalAngle() {
         return 0;
     }
 
+    /**
+     * Set the background sound, which loops whenever the scene is selected
+     * @param {spnr.Sound} sound 
+     */
     setBackgroundSound(sound) {
+        /**
+         * Sound that loops whenever the scene is selected
+         * @member
+         * @readonly
+         * @type {spnr.Sound}
+         */
         this.backgroundSound = sound;
         spnr.internalLog('Does this need a copy or something?');
 
@@ -21,18 +46,28 @@ spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
         }
     }
 
+    /**
+     * @private
+     */
     startBackgroundSound() {
         if (this.backgroundSound != null) {
             this.backgroundSound.loop();
         }
     }
 
+    /**
+     * @private
+     */
     stopBackgroundSound() {
         if (this.backgroundSound != null) {
             this.backgroundSound.stop();
         }
     }
 
+    /**
+     * Add a child to the scene
+     * @param {spnr.GameEngine.Entity} child 
+     */
     addChild(child) {
         var inheritedFunc = spnr.GameEngine.Entity.prototype.addChild.bind(this);
         var childAdded = inheritedFunc(child);
@@ -42,11 +77,19 @@ spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
         }
     }
 
+    /**
+     * Method run when the scene is selected. Override to use.
+     * @virtual
+     */
     onSelected() {
-        // Overwrite
+
     }
 
-    /** Do not call this directly, call through spnr.GameEngine.selectScene() */
+    /**
+     * Do not call directly, call through spnr.GameEngine.selectScene
+     * @param {PIXI.Application} pixiApp 
+     * @private
+     */
     select(pixiApp) {
         this.isSelected = true;
 
@@ -58,11 +101,17 @@ spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
         this.onSelected();
     }
 
+    /**
+     * Method run when the scene is deselected. Override to use.
+     * @virtual
+     */
     onDeselected() {
-        // Overwrite
+        
     }
-
-    /** Do not call this directly, call through spnr.GameEngine.deselectCrntScene() */
+    /**
+     * Do not call directly, call through spnr.GameEngine.deselectCrntScene or spnr.GameEngine.selectScene(aSceneOtherThanThisOne)
+     * @private
+     */
     deselect() {
         this.isSelected = false;
         this.parentAppPointer.stage.removeChild(this.pixiContainer);
@@ -72,10 +121,18 @@ spnr.GameEngine.Scene = class extends spnr.GameEngine.Entity {
         this.onDeselected();
     }
 
+    /**
+     * Override of setParent to make it just parent the game engine
+     * @param {spnr.GameEngine} gameEngine 
+     * @private
+     */
     setParent(gameEngine) {
         this.parent = gameEngine;
     }
 
+    /**
+     * @private
+     */
     internalUpdate() {
         this.updateChildren();
         this.update();
--- src/GameEngine/Texture.js
+++ src/GameEngine/Texture.js
@@ -1,5 +1,15 @@
+/**
+ * Actually just a PIXI texture.
+ * @class
+ */
 spnr.GameEngine.Texture = {};
 
+/**
+ * Load a texture from a URL
+ * @param {string} url 
+ * @returns {spnr.GameEngine.Texture}
+ * @static
+ */
 spnr.GameEngine.Texture.fromUrl = function(url) {
     return PIXI.Texture.from(url);
 }
