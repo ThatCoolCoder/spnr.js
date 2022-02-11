6e32dd1e2a0adaacbbe11a47e6e193368173c2d2
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-02-12 10:00:00 +1030

    Docs for drawable entity

--- src/GameEngine/CircleCollider.js
+++ src/GameEngine/CircleCollider.js
@@ -1,10 +1,25 @@
+/**
+ * Circular collider type. Currently the only collider implemented
+ * @class
+ */
 spnr.GameEngine.CircleCollider = class extends spnr.GameEngine.BaseCollider {
+    /**
+     * Create a new collider
+     * @param {string} name 
+     * @param {spnr.Vector} localPosition 
+     * @param {number} radius 
+     */
     constructor(name, localPosition, radius) {
         super(name, spnr.GameEngine.colliderTypes.circle, localPosition, 0);
 
         this.radius = radius;
     }
 
+    /**
+     * Currently the only collision method implemented.
+     * @param {spnr.GameEngine.BaseCollider} collider 
+     * @returns {boolean}
+     */
     isTouching(collider) {
         switch(collider.type) {
             case spnr.GameEngine.colliderTypes.circle:
